323cf9e1e5079f8df82a2a47cb2ffc28c1821d59
Tanmay Goel <tgoel@ncsu.edu>
2018-02-15 11:20:59 -0500

    workshop code

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var fs = require('fs'),
     xml2js = require('xml2js'),
-    child  = require('child_process'); 
+    child  = require('child_process');
 var parser = new xml2js.Parser();
 var Bluebird = require('bluebird')
 
 var testReport =  '/simplecalc/target/surefire-reports/TEST-com.github.stokito.unitTestExample.calculator.CalculatorTest.xml';
 
-calculatePriority();
-//findFlaky();
+// calculatePriority();
+findFlaky();
 
 async function findFlaky()
 {
+    stats = {}
     for( var i = 0; i < 20; i++ )
     {
         try{
@@ -19,8 +20,18 @@ async function findFlaky()
         var contents = fs.readFileSync(__dirname + testReport)
         let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
         var tests = readResults(xml2json);
-        tests.forEach( e => console.log(i, e));
+        tests.forEach( e => {
+          if (!(e.name in stats)){
+              stats[e.name] = {"passed" : 0, "failed" : 0, "flakyness" : 0};
+          }
+          stats[e.name][e.status] += 1;
+          if(stats[e.name]["failed"] > 0 && stats[e.name]["passed"] > 0){
+              stats[e.name]["flakyness"] = stats[e.name]["failed"]/(stats[e.name]["failed"] + stats[e.name]["passed"])
+          }
+        });
     }
+    console.log(stats);
+
 }
 
 function readResults(result)
@@ -29,13 +40,13 @@ function readResults(result)
     for( var i = 0; i < result.testsuite['$'].tests; i++ )
     {
         var testcase = result.testsuite.testcase[i];
-        
+
         tests.push({
-        name:   testcase['$'].name, 
-        time:   testcase['$'].time, 
+        name:   testcase['$'].name,
+        time:   testcase['$'].time,
         status: testcase.hasOwnProperty('failure') ? "failed": "passed"
         });
-    }    
+    }
     return tests;
 }
 
@@ -47,5 +58,22 @@ async function calculatePriority()
     var contents = fs.readFileSync(__dirname + testReport)
     let xml2json = await Bluebird.fromCallback(cb => parser.parseString(contents, cb));
     var tests = readResults(xml2json);
-    tests.forEach( e => console.log(e));
+
+    var copy = tests.slice(0);
+    copy.sort(function(a,b) {
+        var x = a.status.toLowerCase();
+        var y = b.status.toLowerCase();
+        if (x != y) {
+          return x < y ? -1 : x > y ? 1 : 0;
+        }
+        else {
+          return a.time < b.time ? -1 : a.time > b.time ? 1 : 0;
+        }
+    });
+    copy.forEach( e => console.log(e))
+    a = []
+    copy.forEach( e => a.push(e.name))
+    console.log(a)
+    return a
+    // tests.forEach( e => console.log(e));
 }
